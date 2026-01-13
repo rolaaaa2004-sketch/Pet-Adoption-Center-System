9547f6292e080df1207544c27117eb891f717f24
Rola Hussein <rolaaaa.2004@gmail.com>
2026-01-13 18:23:04 +0200

    Add files via upload

--- pets.php
+++ pets.php
@@ -1,13 +1,26 @@
 <?php
 include "db.php";
-$result = $conn->query("SELECT * FROM pets WHERE status='available'");
+
+$result = $conn->query("SELECT * FROM pets");
+
+if (!$result) {
+    die("Query Error: " . $conn->error);
+}
 ?>
 
 <h2>Available Pets</h2>
 
-<?php while ($pet = $result->fetch_assoc()) { ?>
-<div>
-    <p><?= $pet['name'] ?> - <?= $pet['breed'] ?></p>
-    <a href="adopt.php?id=<?= $pet['id'] ?>">Adopt</a>
-</div>
-<?php } ?>
+<?php
+if ($result->num_rows > 0) {
+    while ($pet = $result->fetch_assoc()) {
+?>
+        <div>
+            <p><?= $pet['name'] ?> - <?= $pet['breed'] ?></p>
+            <a href="adopt.php?id=<?= $pet['id'] ?>">Adopt</a>
+        </div>
+<?php
+    }
+} else {
+    echo "<p>No pets available</p>";
+}
+?>
